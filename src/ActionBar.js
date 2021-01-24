2bcdcdabcc0a338be72ea8463a5fc91ceab6db2e
Jeshua Lin <jeslinmx@users.noreply.github.com>
2021-01-24 23:50:33 +0800

    octocat button looks weird...

--- src/ActionBar.js
+++ src/ActionBar.js
@@ -4,36 +4,51 @@ import { noop, TooltipButton } from "./Misc";
 
 // github
 export default function ActionBar({
-    disableUndo,
-    disableRedo,
-    onUndo = noop,
-    onRedo = noop,
+  disableUndo,
+  disableRedo,
+  onUndo = noop,
+  onRedo = noop,
 }) {
-    return (
-        <Navbar
-            bg='light' variant='light' fixed='top'
-            collapseOnSelect expand='md'
-        >
-            <Nav>
-                <NavDropdown drop='down' title='yakboard'>
-                    <NavDropdown.Item href='#'>Nothing here yet...</NavDropdown.Item>
-                </NavDropdown>
-            </Nav>
-            <Form inline className='ml-2'>
-                <ButtonGroup>
-                    <TooltipButton variant='link' size='sm' placement='bottom' tooltip='Undo' disabled={disableUndo} onClick={onUndo}><ArrowCounterclockwise /></TooltipButton>
-                    <TooltipButton variant='link' size='sm' placement='bottom' tooltip='Redo' disabled={disableRedo} onClick={onRedo}><ArrowClockwise /></TooltipButton>
-                </ButtonGroup>
-            </Form>
-            <Form inline className='ml-auto'>
-                <InputGroup className='ml-2'>
-                    <Form.Control type='text' placeholder='Filter' />
-                    <InputGroup.Append>
-                        <InputGroup.Text><FunnelFill /></InputGroup.Text>
-                    </InputGroup.Append>
-                </InputGroup>
-                <TooltipButton variant='light' placement='bottom' tooltip='View on GitHub' href='https://github.com/jeslinmx/yakboard' className='ml-2'><Github /></TooltipButton>
-            </Form>
-        </Navbar>
-    );
+  return (
+    <Navbar
+      bg='light' variant='light' fixed='top'
+      collapseOnSelect expand='md'
+    >
+      <Nav>
+        <NavDropdown drop='down' title='yakboard'>
+          <NavDropdown.Item href='#'>Nothing here yet...</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+      <Form inline className='ml-2'>
+        <ButtonGroup>
+          <TooltipButton
+            variant='link' size='sm'
+            tooltip='Undo' placement='bottom'
+            disabled={disableUndo} onClick={onUndo}
+            children={<ArrowCounterclockwise />}
+          />
+          <TooltipButton
+            variant='link' size='sm'
+            tooltip='Redo' placement='bottom'
+            disabled={disableRedo} onClick={onRedo}
+            children={<ArrowClockwise />}
+          />
+        </ButtonGroup>
+      </Form>
+      <Form inline className='ml-auto'>
+        <InputGroup className='ml-2'>
+          <Form.Control type='text' placeholder='Filter' />
+          <InputGroup.Append>
+            <InputGroup.Text><FunnelFill /></InputGroup.Text>
+          </InputGroup.Append>
+        </InputGroup>
+        <TooltipButton
+          href='https://github.com/jeslinmx/yakboard'
+          variant='light' size='sm' className='ml-2'
+          tooltip='View on GitHub' placement='bottom'
+          children={<Github />}
+        />
+      </Form>
+    </Navbar>
+  );
 }
